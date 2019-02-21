1c24f283f847db0b3aab673c1685a07eaf312b16
zhuhuiming <1294206499@qq.com>
2019-02-21 20:11:00 +0800

    一般提交-VS

---
+++ spring1718_assignment2_v2/learn_keras.py
@@ -0,0 +1,45 @@
+import numpy as np
+import tensorflow as tf
+
+# 加载数据
+cifar10 = tf.keras.datasets.cifar10.load_data()
+(X_train, y_train), (X_test, y_test) = cifar10
+X_train = X_train.astype(np.float32)
+y_train = y_train.astype(np.int32).flatten()
+X_test = X_test.astype(np.float32)
+y_test = y_test.astype(np.int32).flatten()
+val_size = 1000
+X_val = X_train[:val_size]
+y_val = y_train[:val_size]
+X_train = X_train[val_size:]
+y_train = y_train[val_size:]
+# y_train =
+print('Trian data size', X_train.shape)
+print('Train labels size', y_train.shape)
+print('Validation data size', X_val.shape)
+print('Validation labels size', y_val.shape)
+print('Test data size', X_test.shape)
+print('Test labels size', y_test.shape)
+# 建立模型
+inputs = tf.keras.Input(shape=(32, 32, 3))
+labels = tf.placeholder(tf.int32, [None])
+is_training = tf.placeholder(tf.bool)
+
+initializer = tf.variance_scaling_initializer(scale=2.0)
+x = tf.layers.Conv2D(64, [5, 5], activation='relu', kernel_initializer=initializer)(inputs)
+x = tf.layers.MaxPooling2D([2, 2], [1, 1])(x)
+x = tf.layers.Conv2D(64, [3, 3], activation='relu', kernel_initializer=initializer)(x)
+x = tf.layers.MaxPooling2D([2, 2], [1, 1])(x)
+x = tf.layers.Flatten()(x)
+x = tf.layers.Dropout()(x)
+x = tf.layers.Dense(100, activation='relu', kernel_initializer=initializer)(x)
+x = tf.layers.Dropout()(x)
+predictions = tf.layers.Dense(10, activation='relu', kernel_initializer=initializer)(x)
+
+model  = tf.keras.Model(inputs=inputs, outputs=predictions)
+model.compile(optimizer=tf.train.MomentumOptimizer(2e-2, 0.9, use_nesterov=True),
+              loss=tf.keras.losses.sparse_categorical_crossentropy,
+              metrics=['accuracy'])
+
+# 训练模型
+model.fit(X_train, y_train, batch_size=640, epochs=10, validation_data=(X_val, y_val))
